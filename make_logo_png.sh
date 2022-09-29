f603aee3470be41f5d288c3e3c9de03457c9ad87
ranguli <hello@joshmurphy.ca>
2022-09-29 12:41:57 -0230

    Fix png logos, script is broken

--- make_logo_png.sh
+++ make_logo_png.sh
@@ -6,6 +6,5 @@ declare -a resolutions=("32" "64" "128" "256" "512" "1024" "2048" "3092")
 for val in ${resolutions[@]}; do
    filename="logo_${val}x.png"
    echo "Exporting ${filename}"
-   svgexport logo/logo.svg logo/${filename} ${val}:${val}
-   #inkscape -z -w ${val} -h ${val} logo/logo.svg logo/${filename}
+   inkscape -z -w ${val} -h ${val} logo/logo.svg testdir/${filename}
 done
